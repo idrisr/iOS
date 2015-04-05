db14790cfd936594481dd53374a026591cfd6e4e
Idris Raja <idris.raja@gmail.com>
2015-04-05 16:06:20 +0700

    init commit of App Delegate

---
+++ BNRHomePwner/BNRAppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  BNRAppDelegate.h
+//  BNRHomePwner
+//
+//  Created by idris on 4/5/15.
+//  Copyright (c) 2015 Idris Raja. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BNRAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
