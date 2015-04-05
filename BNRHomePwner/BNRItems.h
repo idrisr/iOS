5a3cc3cf13d6239dbd63c386e313d5c500768821
Idris Raja <idris.raja@gmail.com>
2015-04-05 15:58:43 +0700

    start creating subclass of UITableViewController

---
+++ BNRHomePwner/BNRItems.h
@@ -0,0 +1,12 @@
+//
+//  BNRItems.h
+//  BNRHomePwner
+//
+//  Created by idris on 4/5/15.
+//  Copyright (c) 2015 Idris Raja. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface BNRItems : NSObject
+@end
---
+++ BNRHomePwner/BNRItems.m
@@ -0,0 +1,14 @@
+//
+//  BNRItems.m
+//  BNRHomePwner
+//
+//  Created by idris on 4/5/15.
+//  Copyright (c) 2015 Idris Raja. All rights reserved.
+//
+
+#import "BNRItems.h"
+
+@implementation BNRItems
+
+
+@end
