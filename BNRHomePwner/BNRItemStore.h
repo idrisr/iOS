615635998f4116d7d01e041fb2075428bdde31ea
Idris Raja <idris.raja@gmail.com>
2015-04-05 16:42:37 +0700

    init commit of Model object BNRItemStore

---
+++ BNRHomePwner/BNRItemStore.h
@@ -0,0 +1,13 @@
+//
+//  BNRItemStore.h
+//  BNRHomePwner
+//
+//  Created by idris on 4/5/15.
+//  Copyright (c) 2015 Idris Raja. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface BNRItemStore : NSObject
+
+@end
---
+++ BNRHomePwner/BNRItemStore.m
@@ -0,0 +1,13 @@
+//
+//  BNRItemStore.m
+//  BNRHomePwner
+//
+//  Created by idris on 4/5/15.
+//  Copyright (c) 2015 Idris Raja. All rights reserved.
+//
+
+#import "BNRItemStore.h"
+
+@implementation BNRItemStore
+
+@end
