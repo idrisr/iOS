678fc4ea0c6c16848e8a5f6f5307385c7d26444b
Idris Raja <idris.raja@gmail.com>
2015-04-05 19:28:30 +0700

    main entry point for app

---
+++ BNRHomePwner/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  BNRHomePwner
+//
+//  Created by idris on 4/5/15.
+//  Copyright (c) 2015 Idris Raja. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "BNRAppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([BNRAppDelegate class]));
+    }
+}
