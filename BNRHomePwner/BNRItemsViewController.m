6191437ab237e6046d390dd74faa6a308d5766f6
Idris Raja <idris.raja@gmail.com>
2015-04-06 15:05:12 +0700

    the view for the two sections

--- BNRHomePwner/BNRItemsViewController.m
+++ BNRHomePwner/BNRItemsViewController.m
@@ -15,29 +15,73 @@
 -(UITableViewCell *)tableView:(UITableView *)tableView
         cellForRowAtIndexPath:(NSIndexPath *)indexPath{
     // Create an instance of UITableViewCell, with default appearance
-
     UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"UITableViewCell"
                                                             forIndexPath:indexPath];
+
     // Set the text on the cell with description of the item
     // that is at the nth index of items, where n = row this cell
     // will appear in on the tableview
 
-    NSArray *items = [[BNRItemStore sharedStore] allItems];
+    // TODO
+    // Does this reuse cell objects when old ones go off screen?
+    NSArray *items;
+
+    switch (indexPath.section) {
+        case 0:
+            items = [BNRItemStore.sharedStore allItemsLessThan50];
+            break;
+
+        case 1:
+            items = [[BNRItemStore sharedStore] allItemsMoreThan50];
+            break;
+
+        default:
+            items = [[BNRItemStore sharedStore] allItems];
+            break;
+    }
     BNRItem *item = items[indexPath.row];
-    cell.textLabel.text = [item description];
+    cell.textLabel.text = [NSString stringWithFormat:@"%@\t%i", item.name, item.valueInDollars];
     return cell;
 }
 
 -(NSInteger)tableView:(UITableView *)tableView
 numberOfRowsInSection:(NSInteger)section{
-    return [[[BNRItemStore sharedStore] allItems] count];
+    switch ((int) section) {
+        case 0:
+            return [[[BNRItemStore sharedStore] allItemsLessThan50] count];
+            break;
+        case 1:
+            return [[[BNRItemStore sharedStore] allItemsMoreThan50] count];
+            break;
+
+        default:
+            return [[[BNRItemStore sharedStore] allItems] count];
+            break;
+    }
+}
+
+-(NSString *)tableView:(UITableView *)tableView
+titleForHeaderInSection:(NSInteger) section {
+    switch ((int) section) {
+        case 0:
+            return @"Items under $50";
+            break;
+        case 1:
+            return @"Items over $50";
+            break;
+
+        default:
+            return @"Fuck";
+            break;
+    }
 }
 
 -(instancetype) init{
+    NSLog(@"In init in it");
     self = [super initWithStyle:UITableViewStyleGrouped];
 
     if (self){
-        for (int i = 0; i < 50; i++) {
+        for (int i = 0; i < 8; i++) {
             [[BNRItemStore sharedStore] createItem];
         }
     }
