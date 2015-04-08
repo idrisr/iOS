ea08d202e78e1f5349d9ee947b6731bfe6a2bdfc
Idris Raja <idris.raja@gmail.com>
2015-04-08 16:10:48 +0700

    hook up labels and textfields from new xib

---
+++ BNRDetailViewController.h
@@ -0,0 +1,13 @@
+//
+//  BNRDetailViewController.h
+//  BNRHomePwner
+//
+//  Created by idris on 4/8/15.
+//  Copyright (c) 2015 Idris Raja. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BNRDetailViewController : UIViewController
+
+@end
---
+++ BNRDetailViewController.m
@@ -0,0 +1,20 @@
+//
+//  BNRDetailViewController.m
+//  BNRHomePwner
+//
+//  Created by idris on 4/8/15.
+//  Copyright (c) 2015 Idris Raja. All rights reserved.
+//
+
+#import "BNRDetailViewController.h"
+
+@interface BNRDetailViewController ()
+@property (weak, nonatomic) IBOutlet UITextField *nameField;
+@property (weak, nonatomic) IBOutlet UITextField *valueField;
+@property (weak, nonatomic) IBOutlet UITextField *serialNumberField;
+@property (weak, nonatomic) IBOutlet UILabel *dateLabel;
+
+@end
+
+@implementation BNRDetailViewController
+@end
